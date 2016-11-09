8647beae9eebb5d6dee69016e8891d0ffe08d3d5
Nathan <jcarra@ymail.com>
2016-11-09 07:21:59 +0100

    that

--- sources/ft_parsing.c
+++ sources/ft_parsing.c
@@ -13,6 +13,13 @@
 t_tetri		**ft_fillit_parsing(char *str)
 {
 	t_tetri	**tetris;
+	int		n;
 
-
+	n = 0;
+	if ((tetris = malloc(sizeof (t_tetri *) * 26)) == NULL)
+	  return (NULL);
+	while (n < 26)
+	  tetris[n++] = NULL;
+	/* --- */
+	return (tetris);
 }
